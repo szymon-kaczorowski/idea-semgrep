187cdf57a795c8f5e84ce8e4bf6cff908b60de57
GitHub Action <action@github.com>
2022-10-12 05:47:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/szymonkaczorowski/ideasemgrep/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/szymonkaczorowski/ideasemgrep/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.szymonkaczorowski.ideasemgrep.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.szymonkaczorowski.ideasemgrep.MyBundle
 
 class MyApplicationService {
 
